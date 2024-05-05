2bc36e4f37424c931766b9b2bd016a89d01c3eb4
thememetsar <mattwiner04@gmail.com>
2024-05-05 13:02:50 -0700

    custom names added

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from fpdf import FPDF
+pdfname = input("Please give your file a name")
+print(f"Generating {pdfname}.pdf")
 
 pdf = FPDF(orientation="P",unit="mm",format="A4")
-pdf.set_title("Test")
+pdf.set_title(f"{pdfname}")
 pdf.set_author("Frostie")
 pdf.add_page()
 
 pdf.set_font("Arial",size=18)
 pdf.cell(200,10,text="Hello", ln=1,align="C")
 pdf.cell(200,10,text="Welcome to tape master version 1")
-pdf.output("test.pdf")
+print("DONE")
+pdf.output(f"{pdfname}.pdf")
